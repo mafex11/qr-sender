fb99122278362752cf511855634af82d81c0e763
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2025-01-01 15:08:00 +0530

    changed qr code

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,6 +1,8 @@
 export async function GET(req) {
     const sessionId = Math.random().toString(36).substr(2, 9);
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/mobile/${sessionId}`;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const url = `${baseUrl}/mobile/${sessionId}`;
+    
     return new Response(JSON.stringify({ sessionId, url }), {
       headers: { "Content-Type": "application/json" },
     });

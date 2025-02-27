fa79ddda4c1c9b69efef024cd90f4995946b8596
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2025-02-27 23:14:43 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,6 +1,6 @@
 export async function GET(req) {
     const sessionId = Math.random().toString(36).substr(2, 9);
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
     const url = `${baseUrl}/mobile/${sessionId}`;
     
     return new Response(JSON.stringify({ sessionId, url }), {
